8d2f0442a98bc1b45dd6d2619a0910616fe1e38e
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-05 21:38:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {
+  'garlic': '1 clove',
+  'onion': '1',
+  'red bell pepper': '1',
+  'ground beef': '1 lb.',
+  'chili seasoning': '1 packet',
+  'black beans': '1 can',
+  'kidney beans': '2 cans',
+  'diced tomatoes': '1 can'
+}
